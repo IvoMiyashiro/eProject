171c74e118d163279b64ff65ee12569f2aff0c38
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-03-28 09:28:48 -0300

    add mobile menu

--- components/ui/MobileMenu/Menu/index.tsx
+++ components/ui/MobileMenu/Menu/index.tsx
@@ -1,7 +1,11 @@
 import Link from 'next/link';
-import { HomeIcon } from 'components/icons';
 
-import { Div, Section, Ul, Li, P, Wrapper } from './styles';
+import { Logo, Button } from 'components/ui';
+import { Searchbar } from '../Searchbar';
+
+import { BlogIcon, CartIcon, CraftIcon, DarkModeIcon, HomeIcon, InfoIcon, TagIcon } from 'components/icons';
+import { Div, Section, Ul, Li, P, Wrapper, ButtonWrapper } from './styles';
+import { lightTheme } from 'styles';
 
 interface Props {
   isMenuOpen: boolean;
@@ -11,15 +15,55 @@ export const Menu = ({ isMenuOpen }: Props) => {
   return (
     <Div isOpen={isMenuOpen}>
       <Section>
-
+        <Logo
+          isLink={true}
+          logoColor={lightTheme.color_tertiary_0}
+          withText={false}
+          size="55px"
+        />
       </Section>
       <Section>
         <Ul>
           <Li>
+            <Searchbar />
+          </Li>
+          <Li>
+            <Link href="/" passHref>
+              <Wrapper>
+                <HomeIcon width="30px" height="25px" />
+                <P>Home</P>
+              </Wrapper>
+            </Link>
+          </Li>
+          <Li>
             <Link href="/home" passHref>
               <Wrapper>
-                <HomeIcon />
-                <P>Home</P>
+                <TagIcon width="30px" height="25px" />
+                <P>Categories</P>
+              </Wrapper>
+            </Link>
+          </Li>
+          <Li>
+            <Link href="/home" passHref>
+              <Wrapper>
+                <CraftIcon width="30px" height="25px" />
+                <P>PC Builder</P>
+              </Wrapper>
+            </Link>
+          </Li>
+          <Li>
+            <Link href="/home" passHref>
+              <Wrapper>
+                <InfoIcon width="30px" height="25px" />
+                <P>About</P>
+              </Wrapper>
+            </Link>
+          </Li>
+          <Li>
+            <Link href="/home" passHref>
+              <Wrapper>
+                <BlogIcon width="30px" height="25px" />
+                <P>Blog</P>
               </Wrapper>
             </Link>
           </Li>
@@ -27,9 +71,29 @@ export const Menu = ({ isMenuOpen }: Props) => {
       </Section>
       <Section>
         <Ul>
-          <Li></Li>
+          <Li>
+            <Wrapper>
+              <CartIcon width="30px" height="25px" />
+              <P>Cart</P>
+            </Wrapper>
+          </Li>
+          <Li>
+            <Wrapper>
+              <DarkModeIcon width="30px" height="25px" />
+              <P>Theme</P>
+            </Wrapper>
+          </Li>
         </Ul>
       </Section>
+      <ButtonWrapper>
+        <Button
+          bgColor={lightTheme.color_tertiary_0}
+          textColor={lightTheme.color_primary_0}
+          bRadius="4px"
+        >
+          Signin
+        </Button>
+      </ButtonWrapper>
     </Div>
   );
 };
--- components/ui/MobileMenu/Menu/styles.ts
+++ components/ui/MobileMenu/Menu/styles.ts
@@ -1,3 +1,4 @@
+import { bp } from './../../../../styles/breakpoints';
 import styled from 'styled-components';
 
 interface Styles {
@@ -6,22 +7,61 @@ interface Styles {
 
 export const Div = styled.div<Styles>`
   background-color: ${props => props.theme.color_primary_0};
+  display: flex;
+  flex-direction: column;
+  gap: 1.5em;
   height: 100%;
+  padding: 1em;
   position: fixed;
-  top: 0;
   right: -275px;
+  top: 0;
   transition: right 0.2s ease-in-out;
   width: 275px;
   z-index: 10;
   ${props => props.isOpen ? 'right: 0' : 'right: -275px'};
+
+  @media (min-width: ${bp.tablet}) {
+    display: none;
+  }
 `;
 
-export const Section = styled.section``;
+export const Section = styled.section`
+  :nth-child(3) {
+    padding-top: 1.5em;
+    border-top: 1px solid ${props => props.theme.color_primary_2};
+  }
+`;
 
-export const Ul = styled.ul``;
+export const Ul = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+`;
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  align-items: center;
+  border-radius: 4px;
+  color: ${props => props.theme.color_ui_text_contrast};
+  cursor: pointer;
+  display: flex;
+  gap: 0.75em;
+  padding: 0.5em;
+  transition: 0.2s;
+  width: 100%;
+
+  :hover {
+    background-color: ${props => props.theme.color_primary_1};
+    transition: 0.2s;
+  }
+`;
 
 export const Li = styled.li``;
 
-export const P = styled.p``;
+export const P = styled.p`
+  font-size: 0.9rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  margin-top: auto;
+  height: 45px;
+`;

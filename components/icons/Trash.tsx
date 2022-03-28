7f89fdb7aa6df07238ced4b5b0e5e7494a20ffdf
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-03-28 13:54:54 -0300

    add trash icon

---
+++ components/icons/Trash.tsx
@@ -0,0 +1,15 @@
+export const TrashIcon = (props: any) => (
+  <svg
+    stroke="currentColor"
+    fill="currentColor"
+    strokeWidth={0}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"
+      stroke="none"
+    />
+    <path d="M9 10h2v8H9zm4 0h2v8h-2z" stroke="none" />
+  </svg>
+);

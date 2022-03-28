ff5dcd89f12fe80ed967d16d551c6053c4e037cb
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-03-28 13:56:12 -0300

    add new colors to themes

--- styles/theme.ts
+++ styles/theme.ts
@@ -7,13 +7,13 @@ export const lightTheme: DefaultTheme = {
   color_secondary_0: '',
   color_tertiary_0: '#FFEF00',
   color_neutral_0: '#C8CCCE',
-  color_ui_background: '',
+  color_ui_background: '#FFFFFF',
   color_ui_ok: '',
   color_ui_warn: '',
   color_ui_danger: '',
   color_ui_text_main: '#232F3D',
   color_ui_text_contrast: '#FFFFFF',
-  color_product_background: '',
+  color_product_background: '#F2F5F8',
   color_product_price: '',
   color_product_fullPrice: ''
 };

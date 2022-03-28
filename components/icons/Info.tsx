1ff9a8beb329cf2d9aad10d3ab14163d3cf15180
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-03-28 09:28:18 -0300

    add new icons

--- components/icons/Info.tsx
+++ components/icons/Info.tsx
@@ -8,7 +8,6 @@ export const InfoIcon = (props: any) => (
   >
     <path
       fill="none"
-      stroke="#000"
       strokeWidth={2}
       d="M15 17c0-3 4-5 4-9s-3-7-7-7-7 3-7 7 4 6 4 9v3c0 2 1 3 3 3s3-1 3-3v-3Zm-6 1h6"
     />

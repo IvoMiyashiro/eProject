3c584e8d7b2702de2e002c021f6451963f7898f9
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-03-29 13:09:42 -0300

    add brand to product interface

--- interfaces/product.ts
+++ interfaces/product.ts
@@ -1,9 +1,10 @@
 export interface IProduct {
   id: string;
-  title: string;
+  brand: string;
   category: string;
-  price: number;
   discountPrice: number;
-  quantity: number;
   imageUrl: string;
+  price: number;
+  quantity: number;
+  title: string;
 }

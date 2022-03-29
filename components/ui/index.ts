5ec977fc0a1cd13a9dbe40c5c0cf70c97772883b
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-03-29 13:03:52 -0300

    add products cards to the barrel file

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -3,3 +3,6 @@ export * from './Button';
 export * from './Logo';
 export * from './Modal';
 export * from './ProductCartCard';
+export * from './ProductGridCard';
+export * from './ProductListCard';
+export * from './AsideFilter';

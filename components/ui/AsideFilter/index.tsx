6bb97bf0a63e09127825a9f9c173639d3bbad426
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-03-30 14:40:19 -0300

    update some dependecies

---
+++ components/ui/AsideFilter/index.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const AsideFilter = () => {
+  return (
+    <Aside>
+      
+    </Aside>
+  );
+};
+
+const Aside = styled.aside`
+  width: 100%;
+  height: 100vh;
+  background-color: #f0f0f0;
+`;
---
+++ components/ui/ProductListCard/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ProductListCard = () => {
+  return (
+    <div>ProductListCard</div>
+  );
+};

32bf9e662740945a7d3c8bb06e2863ec671783a9
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-03-30 23:00:35 -0300

    remove products array

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,85 +1,14 @@
 import type { NextPage } from 'next';
+import { useProducts } from 'hooks';
 
 import { MainLayout } from 'components/layouts';
 import { Catalog } from 'components/sections';
 
-import { IProduct } from 'interfaces';
 import styled from 'styled-components';
 
 const ProductsPage: NextPage = () => {
 
-  const productList: IProduct[] = [
-    {
-      id: '1',
-      title: 'GIGABYTE Eagle OC GeForce RTX 3060 Ti 8GB GDDR6 PCI Express 4.0 ATX Video Card GV-N306TEAGLE OC-8GD (rev. 2.0) (LHR)',
-      category: 'Video Cards',
-      quantity: 1,
-      price: 1000,
-      discountPrice: 500,
-      brand: 'GIGABYTE',
-      imageUrl: 'https://i.ibb.co/ZL76FZ9/gigabyte-rtx-3070-removebg-preview.png'
-    },
-    {
-      id: '2',
-      title: 'GIGABYTE Eagle OC GeForce RTX 3060 Ti 8GB GDDR6 PCI Express 4.0 ATX Video Card GV-N306TEAGLE OC-8GD (rev. 2.0) (LHR)',
-      category: 'Video Cards',
-      quantity: 1,
-      price: 1000,
-      discountPrice: 500,
-      brand: 'GIGABYTE',
-      imageUrl: 'https://i.ibb.co/ZL76FZ9/gigabyte-rtx-3070-removebg-preview.png'
-    },
-    {
-      id: '3',
-      title: 'GIGABYTE Eagle OC GeForce RTX 3060 Ti 8GB GDDR6 PCI Express 4.0 ATX Video Card GV-N306TEAGLE OC-8GD (rev. 2.0) (LHR)',
-      category: 'Video Cards',
-      quantity: 1,
-      price: 1000,
-      discountPrice: 500,
-      brand: 'GIGABYTE',
-      imageUrl: 'https://i.ibb.co/ZL76FZ9/gigabyte-rtx-3070-removebg-preview.png'
-    },
-    {
-      id: '4',
-      title: 'GIGABYTE Eagle OC GeForce RTX 3060 Ti 8GB GDDR6 PCI Express 4.0 ATX Video Card GV-N306TEAGLE OC-8GD (rev. 2.0) (LHR)',
-      category: 'Video Cards',
-      quantity: 1,
-      price: 1000,
-      discountPrice: 500,
-      brand: 'GIGABYTE',
-      imageUrl: 'https://i.ibb.co/ZL76FZ9/gigabyte-rtx-3070-removebg-preview.png'
-    },
-    {
-      id: '5',
-      title: 'GIGABYTE Eagle OC GeForce RTX 3060 Ti 8GB GDDR6 PCI Express 4.0 ATX Video Card GV-N306TEAGLE OC-8GD (rev. 2.0) (LHR)',
-      category: 'Video Cards',
-      quantity: 1,
-      price: 1000,
-      discountPrice: 500,
-      brand: 'GIGABYTE',
-      imageUrl: 'https://i.ibb.co/ZL76FZ9/gigabyte-rtx-3070-removebg-preview.png'
-    },
-    {
-      id: '6',
-      title: 'GIGABYTE Eagle OC GeForce RTX 3060 Ti 8GB GDDR6 PCI Express 4.0 ATX Video Card GV-N306TEAGLE OC-8GD (rev. 2.0) (LHR)',
-      category: 'Video Cards',
-      quantity: 1,
-      price: 1000,
-      discountPrice: 500,
-      brand: 'GIGABYTE',
-      imageUrl: 'https://i.ibb.co/ZL76FZ9/gigabyte-rtx-3070-removebg-preview.png'
-    },
-    {
-      id: '7',
-      title: 'GIGABYTE Eagle OC GeForce RTX 3060 Ti 8GB GDDR6 PCI Express 4.0 ATX Video Card GV-N306TEAGLE OC-8GD (rev. 2.0) (LHR)',
-      category: 'Video Cards',
-      quantity: 1,
-      price: 1000,
-      discountPrice: 500,
-      brand: 'GIGABYTE',
-      imageUrl: 'https://i.ibb.co/ZL76FZ9/gigabyte-rtx-3070-removebg-preview.png'
-    },
-  ];
+  const { products, isLoading } = useProducts('/products');
 
   return (
     <MainLayout title="eProject | Our Products" description="">
@@ -90,7 +19,7 @@ const ProductsPage: NextPage = () => {
             <Underline />
           </Wrapper>
         </Section>
-        <Catalog productList={productList}/>
+        <Catalog productList={products} isLoading={isLoading} />
       </Div>
     </MainLayout>
   );

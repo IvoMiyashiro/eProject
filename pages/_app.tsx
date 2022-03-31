5d2e7e31071ac05d9713398ffd4da6e390f6989a
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-03-30 22:55:37 -0300

    add swr for data feching

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,35 @@
 import { useState } from 'react';
 import { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
 
 import { UiProvider } from 'context';
 import { Navbar } from 'components/ui';
+import { Cart } from 'components/ui/Cart';
+import { MobileMenu } from 'components/ui/MobileMenu';
 
+import GlobalStyle from 'styles/global';
 import { DefaultTheme, ThemeProvider } from 'styled-components';
 import { lightTheme } from 'styles';
-import GlobalStyle from 'styles/global';
-import { MobileMenu } from 'components/ui/MobileMenu';
-import { Cart } from 'components/ui/Cart';
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   const [appTheme, setAppTheme] = useState<DefaultTheme>(lightTheme);
 
   return (
-    <ThemeProvider theme={appTheme}>
-      <UiProvider>
-        <Navbar handleAppTheme={setAppTheme} />
-        <MobileMenu />
-        <Cart />
-        <Component {...pageProps} />
-        <GlobalStyle />
-      </UiProvider>
-    </ThemeProvider>
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}>
+      <ThemeProvider theme={appTheme}>
+        <UiProvider>
+          <Navbar handleAppTheme={setAppTheme} />
+          <MobileMenu />
+          <Cart />
+          <Component {...pageProps} />
+          <GlobalStyle />
+        </UiProvider>
+      </ThemeProvider>
+    </SWRConfig>
   );
 }
 

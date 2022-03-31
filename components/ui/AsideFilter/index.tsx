4edacf572942b1538757b9227e7144047690342d
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-03-30 22:56:17 -0300

    add brands to filter

--- components/ui/AsideFilter/index.tsx
+++ components/ui/AsideFilter/index.tsx
@@ -1,15 +1,56 @@
-import styled from 'styled-components';
+import { BrandList, IProduct } from 'interfaces';
+import { useEffect, useState } from 'react';
+import { uniqueArr } from 'utils';
+import { Aside, Div, H3, Section, Input, Label } from './styles';
 
-export const AsideFilter = () => {
+interface Props {
+  productList: IProduct[];
+}
+
+export const AsideFilter = ({ productList }: Props) => {
+
+  const [brandsList, setBrandsList] = useState<BrandList[]>();
+
+  useEffect(() => {
+    const brands = productList.map(product => {
+      return product.brand;
+    });
+
+    setBrandsList(uniqueArr(brands));
+  }, [productList]);
+
   return (
     <Aside>
-      
+      <Div>
+        <H3>Stock</H3>
+        <Section>
+          <Label>           
+            <Input 
+              type="checkbox"
+            />
+            Exclude out of stock items.
+          </Label>
+        </Section>
+      </Div>
+      <Div>
+        <H3>Manufacturer</H3>
+        {
+          brandsList?.map((brand, i) => {
+            return (
+              <Section key={i}>
+                <Label>           
+                  <Input 
+                    type="checkbox"
+                  />
+                  { brand }
+                </Label>
+              </Section>
+            );
+          })
+        }
+
+      </Div>
     </Aside>
   );
 };
 
-const Aside = styled.aside`
-  width: 100%;
-  height: 100vh;
-  background-color: #f0f0f0;
-`;
---
+++ components/ui/AsideFilter/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Aside = styled.aside`
+  box-shadow:
+  0px 0px 1.1px rgba(0, 0, 0, 0.044),
+  0px 0px 3.8px rgba(0, 0, 0, 0.066),
+  0px 0px 17px rgba(0, 0, 0, 0.11);
+  border-radius: 4px;
+  height: 100vh;
+  padding: 1em;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1.25em;
+`;
+
+export const Div = styled.div`
+
+`;
+
+export const H3 = styled.h3`
+  font-size: 1rem;
+  font-weight: 600;
+`;
+
+export const Section = styled.section`
+  margin-top: 0.75em;
+`;
+
+export const Input = styled.input`
+
+`;
+
+export const Label = styled.label`
+  align-items: center;
+  cursor: pointer;
+  display: flex;
+  font-size: 0.8rem;
+  gap: 0.5em;
+`;

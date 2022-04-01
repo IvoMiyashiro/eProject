ea6ea2d97f69a489337a45648d2a49c31119569c
Ivan <ivo.miyashiro1@gmail.com>
2022-03-31 22:52:33 -0300

    fix styles to AsideFilter

--- components/ui/AsideFilter/styles.ts
+++ components/ui/AsideFilter/styles.ts
@@ -1,22 +1,19 @@
 import styled from 'styled-components';
 
 export const Aside = styled.aside`
+  border-radius: 4px;
   box-shadow:
   0px 0px 1.1px rgba(0, 0, 0, 0.044),
   0px 0px 3.8px rgba(0, 0, 0, 0.066),
   0px 0px 17px rgba(0, 0, 0, 0.11);
-  border-radius: 4px;
-  height: 100vh;
-  padding: 1em;
-  width: 100%;
   display: flex;
   flex-direction: column;
   gap: 1.25em;
+  padding: 1.25em;
+  width: 100%;
 `;
 
-export const Div = styled.div`
-
-`;
+export const Div = styled.div``;
 
 export const H3 = styled.h3`
   font-size: 1rem;
@@ -28,13 +25,20 @@ export const Section = styled.section`
 `;
 
 export const Input = styled.input`
-
+  margin-right: 0.25em;
 `;
 
 export const Label = styled.label`
   align-items: center;
   cursor: pointer;
-  display: flex;
+  display: inline-flex;
   font-size: 0.8rem;
-  gap: 0.5em;
+  gap: 0.25em;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  gap: 1em;
+  height: 30px;
+  margin-top: 1em;
 `;

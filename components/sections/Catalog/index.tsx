46783d41bc11b3e1615d417f31498592c60683db
Ivan <ivo.miyashiro1@gmail.com>
2022-04-04 13:12:35 -0300

    add section links

--- components/sections/Catalog/index.tsx
+++ components/sections/Catalog/index.tsx
@@ -1,53 +1,63 @@
 import { useContext } from 'react';
+import Link from 'next/link';
 
 import { CatalogContext } from 'context';
 import { AsideFilter, Modal, ProductGridCard, ProductListCard, Spinner } from 'components/ui';
 import { Header } from './Header';
 
-import { Div, Wrapper, SpinnerWrapper, ProductListAsGrid, ProductListAsList } from './styles';
+import { Div, Wrapper, SpinnerWrapper, ProductListAsGrid, ProductListAsList, Section, A, P } from './styles';
 
 export const Catalog = () => {
 
   const { productList, isLoading, display, isFilterMenuOpen, toggleFilterMenu } = useContext(CatalogContext);
 
   return (
-    
-    <Div>
-      <Wrapper>
-        <AsideFilter />
-      </Wrapper>
-      <Wrapper>
-        <Header />
-        {
-          isLoading
-            ? (
-              <SpinnerWrapper>
-                <Spinner />
-              </SpinnerWrapper>
-            )
-            : display === 'grid'
+    <>
+      <Section>
+        <Link href="/" passHref>
+          <A>Home</A>
+        </Link>
+        <P> &#62; </P>
+        <Link href="/products" passHref>
+          <A>Products</A>
+        </Link>
+      </Section>
+      <Div>
+        <Wrapper>
+          <AsideFilter />
+        </Wrapper>
+        <Wrapper>
+          <Header />
+          {
+            isLoading
               ? (
-                <ProductListAsGrid>
-                  {
-                    productList.map(product => {
-                      return <ProductGridCard key={product.id} product={product} />;
-                    })
-                  }
-                </ProductListAsGrid>
+                <SpinnerWrapper>
+                  <Spinner />
+                </SpinnerWrapper>
               )
-              : (
-                <ProductListAsList>
-                  {
-                    productList.map(product => {
-                      return <ProductListCard key={product.id} product={product} />;
-                    })
-                  }
-                </ProductListAsList>
-              )
-        }
-      </Wrapper>
-      {
-        isFilterMenuOpen
+              : display === 'grid'
+                ? (
+                  <ProductListAsGrid>
+                    {
+                      productList.map(product => {
+                        return <ProductGridCard key={product.id} product={product} />;
+                      })
+                    }
+                  </ProductListAsGrid>
+                )
+                : (
+                  <ProductListAsList>
+                    {
+                      productList.map(product => {
+                        return <ProductListCard key={product.id} product={product} />;
+                      })
+                    }
+                  </ProductListAsList>
+                )
+          }
+        </Wrapper>
+        {
+          isFilterMenuOpen
         &&
         <Modal
           handleCloseChildren={toggleFilterMenu}
@@ -55,9 +65,8 @@ export const Catalog = () => {
         >
           <AsideFilter />
         </Modal>
-      }
-    </Div>
-
-
+        }
+      </Div>
+    </>
   );
 };
--- components/sections/Catalog/styles.ts
+++ components/sections/Catalog/styles.ts
@@ -5,7 +5,6 @@ export const Div = styled.div`
   display: grid;
   grid-template-columns: 275px 1fr;
   gap: 3em;
-  margin-top: 5em;
   align-items: flex-start;
 
   @media (max-width: ${bp.desktop}) {
@@ -51,4 +50,26 @@ export const SpinnerWrapper = styled.div`
   align-items: center;
   justify-content: center;
   min-height: 100vh;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  gap: 0.5em;
+  margin-top: 5em;
+  margin-bottom: 1em;
+  align-items: center;
+`;
+
+export const A = styled.a`
+  text-decoration: none;
+  color: ${props => props.theme.color_primary_2};
+  font-size: 0.9rem;
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+export const P = styled.p`
+  margin-top: -0.15em;
 `;

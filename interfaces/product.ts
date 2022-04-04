b68d43299bb0a4b71d36a8dca90a153aabd322cd
Ivan <ivo.miyashiro1@gmail.com>
2022-04-04 13:16:01 -0300

    add product cart interface

--- interfaces/product.ts
+++ interfaces/product.ts
@@ -1,21 +1,25 @@
 import { BrandList, CategoryList } from './';
 
 export interface IProduct {
-  id: string;
-  title: string;
-  price: number;
+  id:             string;
+  title:          string;
+  price:          number;
   discount_price: number;
-  description: string;
-  stock: number;
-  category: CategoryList;
-  brand: BrandList;
-  image_urls: string[] | string;
-  created_at: Date;
-  updated_at: Date;
+  description:    string;
+  stock:          number;
+  category:       CategoryList;
+  brand:          BrandList;
+  image_urls:     string[] | string;
+  created_at:     Date;
+  updated_at:     Date;
 }
 
 export interface IProductImages {
-  id: string;
+  id:         string;
   product_id: string;
-  imageUrl: string;
+  imageUrl:   string;
+}
+
+export interface IProductCart extends IProduct {
+  quantity: number;
 }

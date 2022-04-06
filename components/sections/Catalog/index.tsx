b2d8e345a6675037fd0b30b9c8ffed61e1691dc8
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-04-06 08:44:17 -0300

    add infinite scroll

--- components/sections/Catalog/index.tsx
+++ components/sections/Catalog/index.tsx
@@ -1,15 +1,25 @@
 import { useContext } from 'react';
 import Link from 'next/link';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 import { CatalogContext } from 'context';
 import { AsideFilter, Modal, ProductGridCard, ProductListCard, Spinner } from 'components/ui';
 import { Header } from './Header';
 
-import { Div, Wrapper, SpinnerWrapper, ProductListAsGrid, ProductListAsList, Section, A, P } from './styles';
+import { Div, Wrapper, SpinnerWrapper, ProductListAsGrid, ProductListAsList, Section, A, P, ScrollSpinnerWrapper } from './styles';
 
 export const Catalog = () => {
 
-  const { productList, isLoading, display, isFilterMenuOpen, toggleFilterMenu } = useContext(CatalogContext);
+  const { 
+    productList,
+    isLoading,
+    display,
+    isFilterMenuOpen,
+    haveMoreProducts,
+    toggleFilterMenu,
+    loadProducts
+  } = useContext(CatalogContext);
+  const offset = productList.reduce(() => (productList.length), 0);
 
   return (
     <>
@@ -35,25 +45,36 @@ export const Catalog = () => {
                   <Spinner />
                 </SpinnerWrapper>
               )
-              : display === 'grid'
-                ? (
-                  <ProductListAsGrid>
-                    {
-                      productList.map(product => {
-                        return <ProductGridCard key={product.id} product={product} />;
-                      })
-                    }
-                  </ProductListAsGrid>
-                )
-                : (
-                  <ProductListAsList>
-                    {
-                      productList.map(product => {
-                        return <ProductListCard key={product.id} product={product} />;
-                      })
-                    }
-                  </ProductListAsList>
-                )
+              : (
+                <InfiniteScroll
+                  dataLength={productList.length}
+                  next={() => loadProducts(offset)}
+                  hasMore={haveMoreProducts}
+                  loader={<ScrollSpinnerWrapper><Spinner /></ScrollSpinnerWrapper>}
+                >
+                  {
+                    display === 'grid'
+                      ? (
+                        <ProductListAsGrid>
+                          {
+                            productList.map(product => {
+                              return <ProductGridCard key={product.id} product={product} />;
+                            })
+                          }
+                        </ProductListAsGrid>
+                      )
+                      : (
+                        <ProductListAsList>
+                          {
+                            productList.map(product => {
+                              return <ProductListCard key={product.id} product={product} />;
+                            })
+                          }
+                        </ProductListAsList>
+                      )
+                  }
+                </InfiniteScroll>
+              )
           }
         </Wrapper>
         {

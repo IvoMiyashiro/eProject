b6f6d300f60a831efe7658add267eca87e05897f
Ivan <ivo.miyashiro1@gmail.com>
2022-04-08 16:11:10 -0300

    add buttons to add and remove product from cart

--- components/ui/ProductListCard/index.tsx
+++ components/ui/ProductListCard/index.tsx
@@ -1,11 +1,13 @@
+import { useContext, useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { CartContext } from 'context';
 import { IProduct } from 'interfaces';
 import { Rating } from '../RatingStars';
-import { CartIcon } from 'components/icons';
+import { CartIcon, CheckIcon, TrashIcon } from 'components/icons';
 
-import { Div, H2, ImageWrapper, Button, Header, InfoWrapper, Price, P, Span, Brand, Section, ImageContainer } from './styles';
+import { AddToCartButton, IsInCartButton, RemoveFromCartButton, Div, H2, ImageWrapper, Header, InfoWrapper, Price, P, Span, Brand, Section, ImageContainer } from './styles';
 
 interface Props {
   product: IProduct;
@@ -13,29 +15,76 @@ interface Props {
 
 export const ProductListCard = ({ product }: Props) => {
 
+  const { cart, addToCart, removeFromCart } = useContext(CartContext);
+  const [isInCart, setInCart] = useState(false);
+  const [isButtonHover, setButtonHover] = useState(false);
+
+  useEffect(() => {
+    const isInCart = cart.some(p => p.id === product.id);
+    setInCart(isInCart);
+  },[cart, product.id]);
+
+  const addProduct = () => {
+    const productToAdd = { ...product, quantity: 1 };
+    addToCart(productToAdd);
+  };
+
   return (
     <Div>
-      <Link href={`/product/${product.id}`} passHref>
-        <ImageWrapper>
+      <ImageWrapper>
+        <Link href={`/products/${product.id}`} passHref>
           <ImageContainer>
             <Image 
-              src={product.image_urls as string}
+              src={product.image_urls[0] as string}
               alt={product.title}
               layout="fill"
               objectFit="contain"
               placeholder="blur"
-              blurDataURL={product.image_urls as string}
+              blurDataURL={product.image_urls[0] as string}
             />
-            <Button>
-              <CartIcon
-                width="22px"
-                height="22px"
-                color="white"
-              />
-            </Button>
           </ImageContainer>
-        </ImageWrapper>
-      </Link>
+        </Link>
+        {
+          isInCart
+            ? (
+              <div 
+                onMouseEnter={() => setButtonHover(true)}
+                onMouseLeave={() => setButtonHover(false)}
+              >
+                {
+                  isButtonHover
+                    ? (
+                      <RemoveFromCartButton onClick={() => removeFromCart(product.id)}>
+                        <TrashIcon
+                          width="22px"
+                          height="22px"
+                          color="white"
+                        />
+                      </RemoveFromCartButton>
+                    )
+                    : (
+                      <IsInCartButton>
+                        <CheckIcon
+                          width="22px"
+                          height="22px"
+                          color="white"
+                        />
+                      </IsInCartButton>
+                    )
+                }
+              </div>
+            )
+            : (
+              <AddToCartButton onClick={addProduct}>
+                <CartIcon
+                  width="22px"
+                  height="22px"
+                  color="white"
+                />
+              </AddToCartButton>
+            )
+        }
+      </ImageWrapper>
       <InfoWrapper>
         <div>
           <Header>
@@ -43,7 +92,7 @@ export const ProductListCard = ({ product }: Props) => {
               {product.brand}
             </Brand>
           </Header>
-          <Link href={`/product/${product.id}`} passHref>
+          <Link href={`/products/${product.id}`} passHref>
             <H2>{product.title}</H2>
           </Link>
           <Section>
--- components/ui/ProductListCard/styles.ts
+++ components/ui/ProductListCard/styles.ts
@@ -23,7 +23,7 @@ export const Price = styled.div`
 export const P = styled.p`
   color: ${props => props.theme.color_primary_0};
   font-family: 'Inter';
-  font-size: 1.75rem;
+  font-size: 1.5rem;
   font-weight: 600;
 
   @media (max-width: ${bp.tablet}) {
@@ -45,19 +45,39 @@ export const Brand = styled.p`
   
 `;
 
-export const Button = styled.button`
-  background-color: ${props => props.theme.color_primary_0};
+const commun = `
+  align-items: center;
   border-radius: 50%;
   border: none;
   cursor: pointer;
+  display: flex;
+  height: 35px;
+  justify-content: center;
   opacity: 0;
-  padding: 0.5em;
+  padding: 0.25em 0.5em;
   position: absolute;
-  right: 6px;
-  top: 6px;
+  right: 14px;
+  top: 14px;
   transition: opacity 0.2s;
+  width: 35px;
   z-index: 1;
 `;
+
+export const AddToCartButton = styled.button`
+  ${commun};
+  background-color: ${props => props.theme.color_primary_0};
+`;
+
+export const IsInCartButton = styled.button`
+  ${commun};
+  background-color: ${props => props.theme.color_ui_ok};
+  `;
+
+export const RemoveFromCartButton = styled.button`
+  ${commun};
+  background-color: ${props => props.theme.color_ui_danger};
+`;
+
 
 export const ImageWrapper = styled.section`
   align-items: center;
@@ -69,6 +89,7 @@ export const ImageWrapper = styled.section`
   height: 175px;
   width: 175px;
   flex-shrink: 0;
+  position: relative;
 
   @media (max-width: ${bp.tablet}) {
     width: 140px;
@@ -98,7 +119,17 @@ export const Div = styled.div`
     transition: transform 0.2s;
   }
   
-  :hover ${Button} {
+  :hover ${AddToCartButton} {
+    opacity: 1;
+    transition: opacity 0.2s;
+  }
+
+  :hover ${IsInCartButton} {
+    opacity: 1;
+    transition: opacity 0.2s;
+  }
+
+  :hover ${RemoveFromCartButton} {
     opacity: 1;
     transition: opacity 0.2s;
   }
@@ -109,7 +140,7 @@ export const H2 = styled.h2`
   -webkit-line-clamp: 2;
   cursor: pointer;
   display: -webkit-box;
-  font-size: 1.1rem;
+  font-size: 1rem;
   font-weight: 600;
   line-height: 1.4em;
   overflow: hidden;

ad76539b44e5bf35586f2ed8c26ef771d9d4f965
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-04-10 17:07:56 -0300

    add get product specs function

---
+++ services/getProductSpecs.ts
@@ -0,0 +1,17 @@
+import { IGpuSpects } from 'interfaces/specs';
+
+interface Response {
+  ok: boolean;
+  specs: IGpuSpects;
+}
+
+export const getProductSpecs = async (product_id: string): Promise<IGpuSpects | []> => {
+
+  const resp = await fetch(`${process.env.BASE_URL}/api/products/${product_id}/specs`);
+  console.log(resp);
+  const { ok, specs }: Response = await resp.json();
+
+  if (!ok) return [];
+
+  return specs;
+};
--- services/index.ts
+++ services/index.ts
@@ -2,3 +2,4 @@ export * from './getProducts';
 export * from './getBrands';
 export * from './getCategories';
 export * from './getProduct';
+export * from './getProductSpecs';

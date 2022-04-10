b775da6b76d936991f97f17a0cc566d6ef6ee318
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-04-10 17:10:01 -0300

    add gpu_specs data

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { Brand, Category, Product, db, initialData } from 'database';
+import { Brand, Category, Product, db, initialData, GpuSpecs } from 'database';
 
 type Data = { ok: boolean, message: string, resp?: any }
 
@@ -9,22 +9,36 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     return res.status(401).json({ ok: false, message: 'Unauthorized'});
   }
 
-  const brands_query = Brand.insert(initialData.brands).returning().toQuery();
-  const category_query = Category.insert(initialData.categories).returning().toQuery();
-  const product_query = Product.insert(initialData.products).returning().toQuery();
+  const brands_query    = Brand.insert(initialData.brands).returning().toQuery();
+  const category_query  = Category.insert(initialData.categories).returning().toQuery();
+  const product_query   = Product.insert(initialData.products).returning().toQuery();
+  const gpu_specs_query = GpuSpecs.insert(initialData.gpu_specs).returning().toQuery();
 
-  const { rows: brands_resp } = await db.conn.query(brands_query);
-  const { rows: category_resp } = await db.conn.query(category_query);
-  const { rows: product_resp } = await db.conn.query(product_query);
+  try {
+    const { rows: brands_resp }    = await db.conn.query(brands_query);
+    const { rows: category_resp }  = await db.conn.query(category_query);
+    const { rows: product_resp }   = await db.conn.query(product_query);
+    const { rows: gpu_specs_resp } = await db.conn.query(gpu_specs_query);
+    
   
+    return res.status(200).json({ 
+      ok: true, 
+      message: 'Done',
+      resp: [
+        brands_resp,
+        category_resp,
+        product_resp,
+        gpu_specs_resp
+      ]
+    });
+  } catch (error) {
+    console.log(error);
 
-  return res.status(200).json({ 
-    ok: true, 
-    message: 'Proceso realizado correctamente',
-    resp: [
-      brands_resp,
-      category_resp,
-      product_resp
-    ]
-  });
+    return res.status(500).json({ 
+      ok: false, 
+      message: 'Internal server error.',
+    });
+  }
+
+
 }

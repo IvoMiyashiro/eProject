efa1881c98f68afdb2c610c843054680ffeeb41e
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-04-13 17:14:28 -0300

    add obj.isEmpty function

--- utils/index.ts
+++ utils/index.ts
@@ -1,1 +1,2 @@
 export * as array from './arrays';
+export * as obj from './object';
---
+++ utils/object/isEmpty.ts
@@ -0,0 +1,8 @@
+export function isEmpty(obj: any) {
+  for(var prop in obj) {
+    if(obj.hasOwnProperty(prop))
+      return false;
+  }
+
+  return true;
+}

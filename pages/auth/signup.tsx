4779e7bf5f9d32d0ddb9037449c5b01900c94acf
Ivan <ivo.miyashiro1@gmail.com>
2022-04-18 16:41:04 -0300

    add signup form

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,14 +1,30 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import styled from 'styled-components';
+
+import { SignupForm } from 'components/ui';
 
 const SignupPage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>eProject | Signup</title>
+        <title>eProject | Signin</title>
       </Head>
+
+      <Div>
+        <SignupForm />
+      </Div>
     </>
   );
 };
 
 export default SignupPage;
+
+export const Div = styled.div`
+  align-items: center;
+  background-color: ${props => props.theme.color_neutral_1};
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  width: 100%;
+`;

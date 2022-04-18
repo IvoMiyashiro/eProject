376b38461e6ef9f6c31ca69b64d62ee4423d3c8a
Ivan <ivo.miyashiro1@gmail.com>
2022-04-18 09:36:05 -0300

    add route when search for a product

--- components/sections/Catalog/index.tsx
+++ components/sections/Catalog/index.tsx
@@ -6,10 +6,12 @@ import { Header } from './Header';
 
 import { Div, Wrapper } from './styles';
 import { ProductList } from './ProductList';
+import { useRouter } from 'next/router';
 
 export const Catalog = () => {
 
   const { isFilterMenuOpen, toggleFilterMenu } = useContext(CatalogContext);
+  const router = useRouter();
   const links = [
     {
       name: 'Home',
@@ -21,9 +23,24 @@ export const Catalog = () => {
     }
   ];
 
+  const searchLinks = [
+    {
+      name: 'Home',
+      link: '/'
+    },
+    {
+      name: 'Products',
+      link: '/products'
+    },
+    {
+      name: `Search: ${router.query.search}`,
+      link: `/products?search=${router.query.search}`
+    }
+  ];
+
   return (
     <>
-      <MapLinks links={links}/>
+      <MapLinks links={router.query.search !== undefined ? searchLinks : links}/>
       <Div>
         <Wrapper>
           <AsideFilter />

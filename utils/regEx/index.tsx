2fb91d90b0e684850fe4c1da596b28ca6c41cf61
Ivan <ivo.miyashiro1@gmail.com>
2022-04-18 16:39:51 -0300

    add regex for full name input

--- utils/regEx/index.tsx
+++ utils/regEx/index.tsx
@@ -1,1 +1,2 @@
 export const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+export const fullNameRegEx = /(^[A-Za-z]{1,16})([ ]{0,1})([A-Za-z]{1,16})?([ ]{0,1})?([A-Za-z]{1,16})?([ ]{0,1})?([A-Za-z]{1,16})/;

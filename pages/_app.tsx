b058338ec8e006165a6a88d7ed5ab9fdceb17741
Ivan <ivo.miyashiro1@gmail.com>
2022-04-18 09:35:47 -0300

    add hoc for next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import { DefaultTheme, ThemeProvider } from 'styled-components';
 
 import { UiProvider, CartProvider } from 'context';
@@ -8,22 +9,24 @@ import { Navbar, Cart, MobileMenu } from 'components/ui';
 import GlobalStyle from 'styles/global';
 import { lightTheme } from 'styles';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 
   const [appTheme, setAppTheme] = useState<DefaultTheme>(lightTheme);
 
   return (
-    <ThemeProvider theme={appTheme}>
-      <UiProvider>
-        <CartProvider>
-          <Navbar handleAppTheme={setAppTheme} />
-          <MobileMenu />
-          <Cart />
-          <Component {...pageProps} />
-          <GlobalStyle />
-        </CartProvider>
-      </UiProvider>
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider theme={appTheme}>
+        <UiProvider>
+          <CartProvider>
+            <Navbar handleAppTheme={setAppTheme} />
+            <MobileMenu />
+            <Cart />
+            <Component {...pageProps} />
+            <GlobalStyle />
+          </CartProvider>
+        </UiProvider>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 

2806039edbb843957dc3c7fbda9b14614e7afe7c
Ivan <ivo.miyashiro1@gmail.com>
2022-04-22 12:49:53 -0300

    add address regEx

--- utils/regEx/index.tsx
+++ utils/regEx/index.tsx
@@ -1,2 +1,3 @@
 export const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 export const fullNameRegEx = /(^[A-Za-z]{1,16})([ ]{0,1})([A-Za-z]{1,16})?([ ]{0,1})?([A-Za-z]{1,16})?([ ]{0,1})?([A-Za-z]{1,16})/;
+export const addressRegEx = /^[#.0-9a-zA-Z\s,-]+$/;

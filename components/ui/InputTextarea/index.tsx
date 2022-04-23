e3dff35bf60f5d3395099c9ba196f585c3940ab6
Ivan <ivo.miyashiro1@gmail.com>
2022-04-23 16:50:18 -0300

    add text area input

---
+++ components/ui/InputTextarea/index.tsx
@@ -0,0 +1,127 @@
+import { ChangeEvent, Dispatch, SetStateAction, useRef, useState } from 'react';
+import { Div, Wrapper, TextArea, Span, Label, InputWrapper } from './styles';
+
+interface Props {
+  placeholder: string;
+  state: {
+    value: string;
+    hasError: boolean;
+    errorMsj: string;
+  };
+  regEx?: RegExp;
+  optional?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  handleStateValue: Dispatch<SetStateAction<{ value: string; hasError: boolean; errorMsj: string;}>>
+}
+
+export const InputTextarea = ({
+  placeholder,
+  state,
+  regEx,
+  optional = false,
+  minLength,
+  maxLength,
+  handleStateValue,
+}: Props) => {
+
+  const { value, hasError, errorMsj } = state;
+  const [isFocus, setFocus] = useState(false);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleFocusInput = () => {
+    inputRef.current?.focus();
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    handleStateValue(prev => {
+      return {
+        ...prev,
+        value: e.target.value
+      };
+    });
+  };
+
+  const handleInputError = () => {
+
+    if (!optional) {
+      if (state.value.length === 0) {
+        return handleStateValue(prev => {
+          return {
+            ...prev,
+            hasError: true,
+            errorMsj: `* ${placeholder} must be filled.`
+          };
+        });
+      }
+    }
+
+    if (!!regEx) {
+      if (!regEx.test(state.value)) {
+        return handleStateValue(prev => {
+          return {
+            ...prev,
+            error: true,
+            errorMsj: `* ${placeholder} is not valid.`
+          };
+        });
+      }
+    }
+
+    if (!!minLength) {
+      if (state.value.length < minLength) {
+        return handleStateValue(prev => {
+          return {
+            ...prev,
+            hasError: true,
+            errorMsj: `* ${placeholder} must be greater than ${minLength + 1}.`
+          };
+        });
+      }
+    }
+
+    if (!!maxLength) {
+      if (state.value.length > maxLength) {
+        return handleStateValue(prev => {
+          return {
+            ...prev,
+            hasError: true,
+            errorMsj: `* ${placeholder} must be less than ${maxLength + 1}.`
+          };
+        });
+      }
+    }
+
+    handleStateValue(prev => {
+      return {
+        ...prev,
+        hasError: false,
+        errorMsj: ''
+      };
+    });
+  };
+
+  return (
+    <InputWrapper>
+      <Div isFocus={isFocus} onClick={handleFocusInput} error={hasError}>
+        <Wrapper>
+          <Label isFocus={isFocus} error={hasError}>
+            { placeholder }
+          </Label>
+          <TextArea
+            value={value}
+            ref={inputRef}
+            onChange={handleInputChange}
+            onFocus={() => setFocus(true)}
+            onBlur={() => {setFocus(false); handleInputError();}}
+            onKeyUp={handleInputError}
+            autoComplete="none"
+          />
+        </Wrapper>
+      </Div>
+
+      { hasError && <Span>{ errorMsj }</Span> }
+    </InputWrapper>
+  );
+};
+
---
+++ components/ui/InputTextarea/styles.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+interface Props {
+  isFocus: boolean;
+  error: boolean
+}
+
+export const InputWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+export const Div = styled.div<Props>`
+  border-radius: 6px;
+  border: 2px solid ${props => 
+    props.error 
+      ? props.theme.color_ui_danger 
+      : props.isFocus
+        ? props.theme.color_primary_0
+        : props.theme.color_neutral_1
+};
+  background-color: ${props => 
+    props.error 
+      ? props.theme.color_ui_danger + '1A'
+      : props.isFocus
+        ? 'transparent'
+        : props.theme.color_neutral_1
+};
+  overflow: hidden;
+  padding: 0.5em;
+  cursor: text;
+  transition: 0.2s;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25em;
+`;
+
+export const Icon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+  
+export const TextArea = styled.textarea`
+  background-color: transparent;
+  border: none;
+  font-size: 0.9rem;
+  outline: none;
+  resize: none;
+`;
+  
+export const Span = styled.span`
+  color: ${props => props.theme.color_ui_danger};
+  font-size: 0.8rem;
+  margin-left: 1em;
+  position: absolute;
+  bottom: -18px;
+`;
+
+export const Label = styled.label<Props>`
+  font-size: 0.75rem;
+  color: ${props => 
+    props.error 
+      ? props.theme.color_ui_danger 
+      : props.isFocus
+        ? props.theme.color_primary_0
+        : props.theme.color_ui_text_main
+};
+  cursor: text;
+`;

e5815f12197d56df4b6d686d418c92da06c692e0
Ivan <ivo.miyashiro1@gmail.com>
2022-04-24 09:29:34 -0300

    add checkout payments form

--- components/ui/CheckoutPaymentsForm/index.tsx
+++ components/ui/CheckoutPaymentsForm/index.tsx
@@ -1,7 +1,73 @@
-import React from 'react';
+import { FormEvent, useState } from 'react';
+import { useRouter } from 'next/router';
+
+import { Button } from 'components/ui';
+import { CashIcon, CreditCard, Bitcoin } from 'components/icons';
+import { InputRadioCard } from '../InputRadioCard';
+
+import { lightTheme } from 'styles';
+import { Form, Div, H1, ButtonWrapper, P } from './styles';
 
 export const CheckoutPaymentsForm = () => {
+
+  const [cashMethod, setCashMethod] = useState(false);
+  const [creditCardMethod, setCreditCardMethod] = useState(false);
+  const [bitconMethod, setBitcoinMethod] = useState(false);
+  const [nextStep, setNextStep] = useState('');
+  const router = useRouter();
+  
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const destination = nextStep === 'Ship to my home' ? '/address' : '/payments';
+    router.push('/checkout' + destination);
+  };
+
   return (
-    <div>CheckoutPaymentsForm</div>
+    <Form onSubmit={handleSubmit}>
+      <H1>How are you going to pay?</H1>
+      <Div>
+        <InputRadioCard
+          name="radio-group"
+          value="Credit Card"
+          icon={<CreditCard width="25px" heigh="25px" />}
+          hisValue={creditCardMethod}
+          price=""
+          handleHisValue={setCreditCardMethod}
+          handleOtherValues={[setBitcoinMethod, setCashMethod]}
+          handleNextStep={setNextStep}
+        />
+        <InputRadioCard
+          name="radio-group"
+          value="Cash"
+          icon={<CashIcon width="25px" heigh="25px" />}
+          hisValue={cashMethod}
+          price=""
+          handleHisValue={setCashMethod}
+          handleOtherValues={[setCreditCardMethod, setBitcoinMethod]}
+          handleNextStep={setNextStep}
+        />
+        <InputRadioCard
+          name="radio-group"
+          value="I'm rich, with Bitcoins"
+          icon={<Bitcoin width="25px" heigh="25px" />}
+          hisValue={bitconMethod}
+          price=""
+          handleHisValue={setBitcoinMethod}
+          handleOtherValues={[setCreditCardMethod, setCashMethod]}
+          handleNextStep={setNextStep}
+        />
+      </Div>
+      <ButtonWrapper>
+        <Button
+          textColor={lightTheme.color_ui_text_contrast}
+          bgColor={lightTheme.color_primary_0}
+          bRadius='4px'
+          disabled={!nextStep}
+          type="submit"
+        >
+          <P>Confirm</P>
+        </Button>
+      </ButtonWrapper>
+    </Form>
   );
 };
---
+++ components/ui/CheckoutPaymentsForm/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Form = styled.form``;
+
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5em;
+`;
+
+export const H1 = styled.h1`
+  font-size: 1.5rem;
+  margin-bottom: 1em;
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 150px;
+  margin-left: auto;
+  height: 40px;
+  margin-top: 1.5em;
+`;
+
+export const P = styled.p`
+  font-size: 0.9rem;
+`;

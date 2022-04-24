5ee5b34966fdb40a633c4740962417314c13e238
Ivan <ivo.miyashiro1@gmail.com>
2022-04-24 16:52:51 -0300

    add regex for visa number, dni and credit card exp date

--- utils/regEx/index.tsx
+++ utils/regEx/index.tsx
@@ -1,3 +1,11 @@
 export const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
 export const fullNameRegEx = /(^[A-Za-z]{1,16})([ ]{0,1})([A-Za-z]{1,16})?([ ]{0,1})?([A-Za-z]{1,16})?([ ]{0,1})?([A-Za-z]{1,16})/;
+
 export const addressRegEx = /^[#.0-9a-zA-Z\s,-]+$/;
+
+export const creditCardDateRegEx = /^(0[1-9]|1[0-2])\/?([0-9]{2})$/;
+
+export const visaNumberRegEx = /^4[0-9]{12}(?:[0-9]{3})?$/;
+
+export const dniRegEx = /^[\d]{1,3}\.?[\d]{3,3}\.?[\d]{3,3}$/;

6321a7d4d62d7485fac9d72d293b8ce73116b418
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-04-27 16:07:44 -0300

    add visa and mastercard testing cards

--- components/ui/Forms/CheckoutForms/CheckoutPaymentsForm/index.tsx
+++ components/ui/Forms/CheckoutForms/CheckoutPaymentsForm/index.tsx
@@ -1,4 +1,5 @@
 import { FormEvent, useState } from 'react';
+import styled from 'styled-components';
 import { useRouter } from 'next/router';
 
 import { CashIcon, CreditCard, Bitcoin } from 'components/icons';
@@ -12,7 +13,8 @@ export const CheckoutPaymentsForm = () => {
   const [isLoading, setLoading] = useState(false);
   const [cashMethod, setCashMethod] = useState(false);
   const [creditCardMethod, setCreditCardMethod] = useState(false);
-  const [bitconMethod, setBitcoinMethod] = useState(false);
+  const [visaTestCard, setVisaTestCard] = useState(false);
+  const [masterTestCard, setMasterTest] = useState(false);
   const [nextStep, setNextStep] = useState('');
   const router = useRouter();
   
@@ -20,7 +22,7 @@ export const CheckoutPaymentsForm = () => {
     e.preventDefault();
     setLoading(true);
     if (nextStep === '') return;
-    const destination = nextStep === 'Credit Card'
+    const destination = nextStep === 'Add new card'
       ? '/credit-card'
       : (nextStep === 'Cash')
         ? '/cash'
@@ -32,36 +34,54 @@ export const CheckoutPaymentsForm = () => {
     <Form onSubmit={handleSubmit}>
       <H1>How are you going to pay?</H1>
       <Div>
-        <InputRadioCard
-          name="radio-group"
-          value="Credit Card"
-          icon={<CreditCard width="25px" heigh="25px" />}
-          hisValue={creditCardMethod}
-          price=""
-          handleHisValue={setCreditCardMethod}
-          handleOtherValues={[setBitcoinMethod, setCashMethod]}
-          handleNextStep={setNextStep}
-        />
-        <InputRadioCard
-          name="radio-group"
-          value="Cash"
-          icon={<CashIcon width="25px" heigh="25px" />}
-          hisValue={cashMethod}
-          price=""
-          handleHisValue={setCashMethod}
-          handleOtherValues={[setCreditCardMethod, setBitcoinMethod]}
-          handleNextStep={setNextStep}
-        />
-        <InputRadioCard
-          name="radio-group"
-          value="I'm rich, with Bitcoins"
-          icon={<Bitcoin width="25px" heigh="25px" />}
-          hisValue={bitconMethod}
-          price=""
-          handleHisValue={setBitcoinMethod}
-          handleOtherValues={[setCreditCardMethod, setCashMethod]}
-          handleNextStep={setNextStep}
-        />
+        <Wrapper>
+          <H2>With credit or debit</H2>
+          <InputRadioCard
+            name="radio-group"
+            value="Visa testing credit card"
+            text='This card allow you to test the payment process'
+            icon={<CreditCard width="25px" heigh="25px" />}
+            hisValue={visaTestCard}
+            price=""
+            handleHisValue={setVisaTestCard}
+            handleOtherValues={[setCashMethod, setMasterTest, setCreditCardMethod]}
+            handleNextStep={setNextStep}
+          />
+          <InputRadioCard
+            name="radio-group"
+            value="Mastercard testing credit card"
+            text='This card allow you to test the payment process'
+            icon={<CreditCard width="25px" heigh="25px" />}
+            hisValue={masterTestCard}
+            price=""
+            handleHisValue={setMasterTest}
+            handleOtherValues={[setCashMethod, setVisaTestCard, setCreditCardMethod]}
+            handleNextStep={setNextStep}
+          />
+          <InputRadioCard
+            name="radio-group"
+            value="Add new card"
+            icon={<CreditCard width="25px" heigh="25px" />}
+            hisValue={creditCardMethod}
+            price=""
+            handleHisValue={setCreditCardMethod}
+            handleOtherValues={[setCashMethod, setVisaTestCard, setMasterTest]}
+            handleNextStep={setNextStep}
+          />
+        </Wrapper>
+        <Wrapper>
+          <H2>With cash</H2>
+          <InputRadioCard
+            name="radio-group"
+            value="Cash"
+            icon={<CashIcon width="25px" heigh="25px" />}
+            hisValue={cashMethod}
+            price=""
+            handleHisValue={setCashMethod}
+            handleOtherValues={[setCreditCardMethod]}
+            handleNextStep={setNextStep}
+          />
+        </Wrapper>
       </Div>
       <ButtonSection 
         disabled={!nextStep}
@@ -70,3 +90,12 @@ export const CheckoutPaymentsForm = () => {
     </Form>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+`;
+const H2 = styled.h2`
+  font-size: 0.9rem;
+`;

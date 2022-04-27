ae08da6cf9f67a4f9ce5d9c08e2effa443bde013
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-04-27 16:10:21 -0300

    remove summary form

--- components/ui/Forms/CheckoutForms/CheckoutSummaryForm/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const CheckoutSummaryForm = () => {
-  return (
-    <div>CheckoutSummary</div>
-  );
-};
--- components/ui/Forms/CheckoutForms/index.ts
+++ components/ui/Forms/CheckoutForms/index.ts
@@ -2,4 +2,3 @@ export * from './CheckoutAddressForm';
 export * from './CheckoutCreditCardForm';
 export * from './CheckoutPaymentsForm';
 export * from './CheckoutShippingForm';
-export * from './CheckoutSummaryForm';

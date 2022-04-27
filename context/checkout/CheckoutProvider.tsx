1b129b44250a9ade72e332de885bd45b8596740b
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-04-27 16:08:36 -0300

    fix context name

--- context/checkout/CheckoutProvider.tsx
+++ context/checkout/CheckoutProvider.tsx
@@ -6,18 +6,18 @@ export interface CheckoutState {
   step: 1 | 2 | 3;
 }
 
-const CART_INIT_STATE: CheckoutState = { 
+const CHECKOUT_INIT_STATE: CheckoutState = { 
   step: 1
 };
 
-export const CartProvider: FC = ({ children }) => {
+export const CheckoutProvider: FC = ({ children }) => {
 
-  const [state, dispatch] = useReducer(checkoutReducer, CART_INIT_STATE);
+  const [state, dispatch] = useReducer(checkoutReducer, CHECKOUT_INIT_STATE);
 
   return (
     <CheckoutContext.Provider value={{
       ...state,
-
+ 
       // Methods
     }}>
       { children }

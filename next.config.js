c4a48e33eb60407bcda00deb3d555df2c429f54c
Ivan <ivo.miyashiro1@gmail.com>
2022-04-29 19:21:52 -0300

    add mercadopago public key

--- next.config.js
+++ next.config.js
@@ -3,10 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     BASE_URL: process.env.BASE_URL,
+    MERCADO_PAGO_PUBLIC_KEY: process.env.MERCADO_PAGO_PUBLIC_KEY
   },
   images: {
     domains: ['c1.neweggimages.com', 'lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
-  },
+  }
 };
 
 module.exports = nextConfig;

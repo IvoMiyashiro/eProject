bdf4fd5f4e30582cdeee83331d0f0187f2b37a7b
Ivan <ivo.miyashiro1@gmail.com>
2022-05-02 18:17:08 -0300

    fix expiration month and year select input

--- components/ui/Forms/CheckoutForms/CheckoutCreditCardForm/Inputs/index.tsx
+++ components/ui/Forms/CheckoutForms/CheckoutCreditCardForm/Inputs/index.tsx
@@ -1,16 +1,20 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useContext } from 'react';
 import { InputControl, InputNumber } from 'components/ui';
 import styled from 'styled-components';
+import { AuthContext } from 'context';
+import { InputSelect } from 'components/ui/Inputs';
 
 interface Props {
   cardholderName: State;
   cardNumber: State;
   expMonth: State;
+  expYear: State;
   cvc: State;
   dni: State;
   handleCardholderNameValue: Dispatch<SetStateAction<{ value: string; hasError: boolean; errorMsj: string; }>>
   handleCardNumberValue: Dispatch<SetStateAction<{ value: string; hasError: boolean; errorMsj: string; }>>
   handleExpMonthValue: Dispatch<SetStateAction<{ value: string; hasError: boolean; errorMsj: string; }>>
+  handleExpYearValue: Dispatch<SetStateAction<{ value: string; hasError: boolean; errorMsj: string; }>>
   handleCvcValue: Dispatch<SetStateAction<{ value: string; hasError: boolean; errorMsj: string; }>>
   handleDniValue: Dispatch<SetStateAction<{ value: string; hasError: boolean; errorMsj: string; }>>
   handleCvcFocus: Dispatch<SetStateAction<boolean>>
@@ -26,26 +30,32 @@ export const Inputs = ({
   cardholderName,
   cardNumber,
   expMonth,
+  expYear,
   cvc,
   dni,
   handleCardholderNameValue,
   handleCardNumberValue,
   handleExpMonthValue,
+  handleExpYearValue,
   handleCvcValue,
   handleDniValue,
   handleCvcFocus
 }: Props) => {
   
+  const { customer } = useContext(AuthContext);
+
   return (
     <Div>
       <Wrapper>
         <InputNumber
+          id="form-checkout__cardNumber"
           type="credit-card"
           placeholder='Credit Card Number'
           state={cardNumber}
           handleStateValue={handleCardNumberValue}
         />
-        <InputControl 
+        <InputControl
+          id="form-checkout__cardholderName"
           type='text'
           placeholder='Cardholder Name'
           state={cardholderName}
@@ -53,13 +63,24 @@ export const Inputs = ({
         />
       </Wrapper>
       <Wrapper>
+        <Section>
+          <InputSelect
+            id="form-checkout__cardExpirationMonth"
+            state={expMonth}
+            placeholder="Expiration month"
+            options={['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']}
+            handleStateValue={handleExpMonthValue}
+          />
+          <InputSelect
+            id="form-checkout__cardExpirationYear"
+            state={expYear}
+            placeholder="Expiration Year"
+            options={['23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34']}
+            handleStateValue={handleExpYearValue}
+          />
+        </Section>
         <InputNumber 
-          type='date'
-          placeholder='Expiration Date'
-          state={expMonth}
-          handleStateValue={handleExpMonthValue}
-        />
-        <InputNumber 
+          id="form-checkout__securityCode"
           type='num'
           maxLength={4}
           placeholder='CVC'
@@ -69,13 +90,20 @@ export const Inputs = ({
         />
       </Wrapper>
       <Wrapper>
-        <InputNumber 
+        <InputNumber
+          id='form-checkout__identificationNumber'
           type='dni'
           placeholder='Cardholder DNI'
           state={dni}
           handleStateValue={handleDniValue}
         />
       </Wrapper>
+      <input hidden type="hidden" name="cardholderEmail" id="form-checkout__cardholderEmail" value={customer!.email}/>
+      <select hidden name="issuer" id="form-checkout__issuer"></select>
+      <select hidden name="identificationType" id="form-checkout__identificationType"></select>
+      <select hidden name="installments" id="form-checkout__installments"></select>
+      <button hidden type="submit" id="form-checkout__submit">Pagar</button>
+      <progress hidden value="0" className="progress-bar">Cargando...</progress>
     </Div>
   );
 };
@@ -88,6 +116,12 @@ const Div = styled.div`
   
 const Wrapper = styled.div`
   display: flex;
+  gap: 2em;
+`;
+
+const Section = styled.section`
+  display: flex;
+  min-width: 306px;
   gap: 2em;
 `;
   

dfe084cf9b8d20f03b07eb2d6b48937ddfd75fce
Ivan <ivo.miyashiro1@gmail.com>
2022-05-03 00:17:51 -0300

    make aside menu close when click checkout button

--- components/ui/Cart/AsideCartMenu/ProductList/index.tsx
+++ components/ui/Cart/AsideCartMenu/ProductList/index.tsx
@@ -1,18 +1,26 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Link from 'next/link';
 
-import { CartContext } from 'context';
+import { CartContext, UiContext } from 'context';
 import { IProductCart } from 'interfaces';
 import { ArrowRightIcon } from 'components/icons';
 import { ProductCartCard } from 'components/ui';
 
 import { A, Div, ProductsWrapper, Footer, Ul, Li, P, H3 } from './styles';
+import { Spinner } from 'components/ui/Spinner';
 
 interface Props { cart: IProductCart[] }
 
 export const ProductList = ({ cart }: Props) => {
 
-  const { orderPrice, orderDiscount, orderTotalPrice } = useContext(CartContext);
+  const { toggleCartMenu } = useContext(UiContext);
+  const { orderPrice, orderDiscount, orderTotalPrice,  } = useContext(CartContext);
+  const [isLoading, setLoading] = useState(false);
+
+  const handleClick = () => {
+    toggleCartMenu();
+    setLoading(true);
+  };
 
   return (
     <>
@@ -40,9 +48,13 @@ export const ProductList = ({ cart }: Props) => {
             </Li>
           </Ul>
           <Link href="/checkout" passHref>
-            <A>
-              Checkout
-              <ArrowRightIcon width="22px" height="22px" />
+            <A onClick={handleClick}>
+              {
+                isLoading
+                  ? <Spinner />
+                  : <> Checkout <ArrowRightIcon width="22px" height="22px" /> </>
+              }
+              
             </A>
           </Link>
         </Footer>

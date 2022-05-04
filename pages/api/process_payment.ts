7a3ffaace63845c46fa702cc2a636d15bab614d4
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-05-04 16:42:27 -0300

    work in update stock function when customer purchase a product

--- pages/api/process_payment.ts
+++ pages/api/process_payment.ts
@@ -1,24 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import mercadopago from 'mercadopago';
 import { db } from 'database';
-import { IOrders } from 'interfaces';
 
 type Data = {
   status?: string,
   status_detail?: string,
   id?: any,
   message?: string;
-  order?: IOrders;
+  order_id?: string;
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   mercadopago.configurations.setAccessToken(process.env.MERCADO_PAGO_SECRET as string);
 
   try {
+    const dbPrice = await checkDBPrice(req, res);
 
-    const totalPrice = await checkPrice(req, res);
-
-    if (totalPrice !== req.body.transaction_amount) {
+    if (dbPrice !== req.body.transaction_amount) {
       return res.status(400).json({
         message: 'Invalid price amount.'
       });
@@ -35,13 +33,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       });
     }
 
-    const order = await createOrder(req, res, totalPrice);
+    const order = await createOrder(req, res, dbPrice);
 
     return res.status(payment.status).json({
       id,
       status,
       status_detail,
-      order
+      order_id: order.id
     });
 
   } catch (error) {
@@ -58,19 +56,28 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>, tota
 
   const query = 'INSERT INTO order_info (id, shipping_method, shipping_address, payment_status, products_id, total_paid, customer_id) VALUES (nextval(\'order_id\'),$1, $2, $3, $4, $5, $6) RETURNING *';
   const values = [shippingMethod, address, 'paid', productsIDs, totalPrice, uid];
-  
+  await updateStock(productsIDs);
   try {
     const { rows } = await db.conn.query(query, values);
-    return rows;
+
+    return rows[0];
   } catch (error) {
     console.log(error);
   }
 };
 
-const checkPrice = async (req: NextApiRequest, res: NextApiResponse<Data>): Promise<any> => {
+const updateStock = async (productsIDs: string[]) => {
 
+  productsIDs.map(id => {
+    const query = 'UPDATE product SET product.stock = product.stock -  WHERE c.member_id = ?';
+  });
+};
+
+const checkDBPrice = async (req: NextApiRequest, res: NextApiResponse<Data>): Promise<any> => {
+  
   let totalPrice = 0;
   const { productsIDs } = JSON.parse(req.body.description);
+
   const query = 'SELECT discount_price FROM product WHERE id = ANY ($1)';
   const values = [productsIDs];
 

11a5351c65fac77e3984f3a4a3f8ba67365f7ac8
Ivan <ivo.miyashiro1@gmail.com>
2022-05-06 09:46:55 -0300

    add fix product search card props

--- components/ui/Cards/ProductSearchCard/index.tsx
+++ components/ui/Cards/ProductSearchCard/index.tsx
@@ -1,24 +1,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { IProduct } from 'interfaces';
 
 import { Div, ImageWrapper, H2, Section, P, Wrapper } from './styles';
 
 interface Props {
-  id: string;
-  image: string;
-  title: string;
-  brand: string;
-  onClick: any;
+  data: IProduct
 }
 
-export const ProductSearchCard = ({ image, title, id, brand, onClick }: Props) => {
+export const ProductSearchCard = ({ data }: Props) => {
+
+  const { image_urls, title, id, brand } = data;
+
   return (
     <Link href={`/products/${id}`} passHref>
-      <Div onClick={onClick}>
+      <Div>
         <Section>
           <ImageWrapper>
             <Image 
-              src={image}
+              src={image_urls[0]}
               alt={title}
               layout="fill"
               objectFit="contain"

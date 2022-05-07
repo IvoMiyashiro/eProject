3fc188e38a8f1489d4a850af41b2ce24a2df629d
Ivan <ivo.miyashiro1@gmail.com>
2022-05-07 18:21:09 -0300

    add orders endpoint

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,3 +1,4 @@
+import { db } from 'database';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = { ok: boolean, message?: string, order?: any }
@@ -5,8 +6,8 @@ type Data = { ok: boolean, message?: string, order?: any }
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 
   switch( req.method ) {
-  case 'POST':
-    return;
+  case 'GET':
+    return getOrders(req, res);
   
   case 'PUT':
     return updateOrder(req, res);
@@ -14,9 +15,35 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   default:
     return res.status(400).json({
       ok: false,
-      message: 'Bad request'
+      message: 'Bad request.'
     });
   }
+};
+
+const getOrders = async (req: NextApiRequest, res: NextApiResponse) => {
+
+  const { customer_id } = req.query;
+
+  const query = 'SELECT * FROM order_info WHERE customer_id = ($1)';
+  const value = [customer_id];
+
+  try {
+    
+    const { rows } = db.conn.query(query, value);
+
+    return res.status(200).json({
+      ok: true,
+      order: rows[0]
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      message: 'Internal Server Error.'
+    });
+  }
+
 };
 
 const updateOrder = async (req: NextApiRequest, res: NextApiResponse) => {

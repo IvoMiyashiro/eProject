7ceee321a32e04cf29e1c32141ee6c23971e33a1
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-05-09 14:01:56 -0300

    add customer orders hook

---
+++ hooks/useCustomerOrders.ts
@@ -0,0 +1,36 @@
+import { useContext, useEffect, useState } from 'react';
+import { getUserOrders } from 'services';
+import { IOrders } from 'interfaces';
+import { AuthContext } from 'context';
+
+export const useCustomerOrders = (
+  limit: number | undefined = undefined,
+  offset: number | undefined = undefined
+) => {
+
+  const { customer } = useContext(AuthContext);
+  const [orders, setOrders] = useState<[] | IOrders[]>([]);
+  const [totalOrders, setTotalOrders] = useState(0);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    const filters = {
+      limit,
+      offset
+    };
+
+    if (customer === undefined) return; 
+    getUserOrders(customer.id, filters).then(resp => {
+      setOrders(resp?.orders || []);
+      setTotalOrders(resp?.totalOrders || 0);
+      setLoading(false);
+    }); 
+  }, [customer, limit, offset]);
+
+  return {
+    orders,
+    isLoading,
+    totalOrders
+  };
+};

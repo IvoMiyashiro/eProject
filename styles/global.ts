0f54d842d9de0581498df09d96354f92da18c2dc
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-05-09 15:52:14 -0300

    add custom scrollbar

--- styles/global.ts
+++ styles/global.ts
@@ -56,4 +56,31 @@ export default createGlobalStyle`
     cursor: pointer;
     outline: none;
   }
+
+  ::-webkit-scrollbar {
+    width: 20px;
+   }
+  ::-webkit-scrollbar-track {
+    background-color: transparent;
+  } 
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #d6dee1;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #d6dee1;
+    border-radius: 20px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #d6dee1;
+    border-radius: 20px;
+    border: 6px solid transparent;
+    background-clip: content-box;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: #a8bbbf;
+  }
 `;

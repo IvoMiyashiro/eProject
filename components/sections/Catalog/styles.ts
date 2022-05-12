1f5a926913be673a0092367805e1ad75998fd729
Ivan <ivo.miyashiro1@gmail.com>
2022-05-11 21:16:17 -0300

    fix padding and gaps

--- components/sections/Catalog/styles.ts
+++ components/sections/Catalog/styles.ts
@@ -4,7 +4,7 @@ import { bp } from 'styles';
 export const Div = styled.div`
   display: grid;
   grid-template-columns: 275px 1fr;
-  gap: 1em;
+  gap: 3em;
   align-items: flex-start;
 
   @media (max-width: ${bp.tablet}) {
@@ -14,17 +14,10 @@ export const Div = styled.div`
 `;
 
 export const Wrapper = styled.div`
-
   :first-child {
     @media (max-width: ${bp.tablet}) {
       display: none;
     }
-  }
-
-  :last-child {
-    overflow-x: auto;
-    padding: 0 2em;
-    padding-bottom: 2em;
   }
 `;
 

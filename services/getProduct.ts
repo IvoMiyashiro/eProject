0288f59a69cd956889d3a64215e56b2de188e46d
Ivan <ivo.miyashiro1@gmail.com>
2022-05-13 16:48:53 -0300

    fix return

--- services/getProduct.ts
+++ services/getProduct.ts
@@ -2,15 +2,15 @@ import { IProduct } from 'interfaces';
 
 interface Response {
   ok: boolean;
-  product: IProduct[]
+  product: IProduct
 }
 
-export const getProductById = async (id: string): Promise<IProduct[]> => {
+export const getProductById = async (id: string): Promise<IProduct | undefined> => {
 
   const resp = await fetch(`${process.env.BASE_URL}/api/products/${id}`);
   const { ok, product }: Response = await resp.json();
 
-  if (!ok) return [];
+  if (!ok) return undefined;
 
   return product;
 };

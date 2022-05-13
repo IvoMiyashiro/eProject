054f865de7f59bb3f8bdcf4a736f271254c321eb
Ivan <ivo.miyashiro1@gmail.com>
2022-05-13 16:50:32 -0300

    remove unused styled-components

--- components/sections/Product/components/Reviews/Header/styles.ts
+++ components/sections/Product/components/Reviews/Header/styles.ts
@@ -21,24 +21,3 @@ export const Wrapper = styled.div`
   display: flex;
   gap: 0.5em;
 `;
-
-export const View = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 0.25em;
-`;
-
-export const Text = styled.p`
-  font-size: 0.9rem;
-`;
-
-export const Select = styled.select`
-  height: 30px;
-  padding: 0 0.25em;
-  font-family: 'Inter';
-`;
-  
-export const Option = styled.option`
-  font-family: 'Inter';
-  height: 30px;
-`;

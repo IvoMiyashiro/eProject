ab877c3191828330d34fd5376f37752f0b5784ac
Ivan <ivo.miyashiro1@gmail.com>
2022-05-18 00:24:22 -0300

    add createReview

--- pages/api/products/[id]/reviews.ts
+++ pages/api/products/[id]/reviews.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { v4 as uuidv4 } from 'uuid';
 import { IReviews } from 'interfaces';
 import { db } from 'database';
 
@@ -9,6 +10,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   switch( req.method ) {
   case 'GET':
     return getReviews(req, res);
+  
+  case 'POST':
+    return createReview(req, res);
 
   default:
     return res.status(400).json({
@@ -39,6 +43,32 @@ const getReviews = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
       totalLength: lengthRows.length
     });
 
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      ok: false,
+      message: 'Internal server error.'
+    });
+  }
+};
+
+const createReview = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+  const { id: product_id } = req.query;
+  const { customer_id, rating, pros, cons, overall } = req.body;
+  const id = uuidv4();
+
+  const query = 'INSERT INTO review (id, customer_id, product_id, rating, pros, cons, overall) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *';
+  const values = [id, customer_id, product_id, rating, pros,cons, overall];
+
+  try {
+    const { rows } = await db.conn.query(query, values);
+
+    return res.status(200).json({
+      ok: true,
+      reviews: rows[0],
+    });
   } catch (error) {
     console.log(error);
 

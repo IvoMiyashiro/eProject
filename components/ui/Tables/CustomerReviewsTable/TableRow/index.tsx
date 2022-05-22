5b544ae67628bce16b54b1d1544dd745c0912929
Ivan <ivo.miyashiro1@gmail.com>
2022-05-21 21:00:40 -0300

    add review info and delete modal

--- components/ui/Tables/CustomerReviewsTable/TableRow/index.tsx
+++ components/ui/Tables/CustomerReviewsTable/TableRow/index.tsx
@@ -3,22 +3,31 @@ import Link from 'next/link';
 import Image from 'next/image';
 import dayjs from 'dayjs';
 
+import { Modal } from 'components/ui';
 import { DotsIcon } from 'components/icons';
+import { ReviewInfo } from '../ReviewInfo';
+import { DeleteReviewForm } from '../DeleteReviewForm';
 
 import { Td, Tr } from '../../styles';
 import { Button, FirstTd, ImageWrapper, LastTd, Li, Menu, P, Ul, Wrapper } from './styles';
 
 interface Props {
+  review_id: string;
   product_id: string;
   title: string;
   created_at: Date;
   rating: string;
   image_url: string;
+  pros: string;
+  cons: string;
+  overall: string;
 }
 
-export const TableRow = ({ product_id, title, created_at, rating, image_url }: Props) => {
+export const TableRow = ({ review_id, product_id, title, created_at, rating, image_url, pros, cons, overall }: Props) => {
 
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [isReviewModalOpen, setReviewModalOpen] = useState(false);
 
   return (
     <Tr>
@@ -48,11 +57,40 @@ export const TableRow = ({ product_id, title, created_at, rating, image_url }: P
             <Wrapper onClick={() => setMenuOpen(false)}/>
             <Menu>
               <Ul>
-                <Li>See more</Li>
-                <Li>Delete</Li>
+                <Li onClick={() => {setReviewModalOpen(true); setMenuOpen(false);}}>See more</Li>
+                <Li onClick={() => {setDeleteModalOpen(true); setMenuOpen(false);}}>Delete</Li>
               </Ul>
             </Menu>
           </>
+        }
+        {
+          isReviewModalOpen
+          &&
+          <Modal
+            handleCloseChildren={() => setReviewModalOpen(false)}
+          >
+            <ReviewInfo 
+              rating={Number(rating)}
+              pros={pros}
+              cons={cons}
+              overall={overall}
+              handleDeleteModalOpen={setDeleteModalOpen}
+            />
+          </Modal>
+        }
+        {
+          isDeleteModalOpen
+          &&
+          <Modal
+            handleCloseChildren={() => setDeleteModalOpen(false)}
+          >
+            <DeleteReviewForm
+              review_id={review_id}
+              product_id={product_id}
+              handleDeleteModalOpen={setDeleteModalOpen}
+              handleReviewModalOpen={setReviewModalOpen}
+            />
+          </Modal>
         }
       </LastTd>
     </Tr>

95e01685f36553056e6398d8b68b3d717a767d7a
Ivan <ivo.miyashiro1@gmail.com>
2022-05-23 16:15:14 -0300

    add profile page

---
+++ pages/profile/index.tsx
@@ -0,0 +1,65 @@
+import { useContext } from 'react';
+import type { NextPage } from 'next';
+import styled from 'styled-components';
+
+import { AuthContext, CutomerReviewsProvider } from 'context';
+import { MainLayout } from 'components/layouts';
+import { CustomerProfile } from 'components';
+
+const CustomerReviewsPage: NextPage = () => {
+
+  const { customer } = useContext(AuthContext);
+
+  return (
+    <>
+      <CutomerReviewsProvider>
+        <MainLayout
+          title={`eProject | ${customer?.name}`} 
+          description=""
+          needAuth
+          onUnauthenticatedPath="/profile"
+        >
+          <Div>
+            <Section>
+              <Wrapper>
+                <H1>My Profile</H1>
+                <Underline />
+              </Wrapper>
+            </Section>
+            <CustomerProfile />
+          </Div>
+        </MainLayout>
+      </CutomerReviewsProvider>
+    </>
+  );
+};
+
+export default CustomerReviewsPage;
+
+const Div = styled.div`
+  padding: 7em 0;
+`;
+
+const Section = styled.section`
+  display: flex;
+  justify-content: center;
+`;
+  
+const H1 = styled.h1`
+  font-size: 2.5rem;
+`;
+  
+const Underline = styled.span`
+  background-color: ${props => props.theme.color_tertiary_0};
+  bottom: -5px;
+  height: 10px;
+  position: absolute;
+  transform: rotate(-1deg);
+  width: 100%;
+`;
+
+const Wrapper = styled.div`
+  display: inline;
+  margin-bottom: 2.5em;
+  position: relative;
+`;

82221e52b31aceb0850b44f1cb168e367336e8f0
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-05-24 12:05:13 -0300

    add cloudinary env and url for images

--- next.config.js
+++ next.config.js
@@ -3,11 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     BASE_URL: process.env.BASE_URL,
-    MERCADO_PAGO_PUBLIC_KEY: process.env.MERCADO_PAGO_PUBLIC_KEY
+    MERCADO_PAGO_PUBLIC_KEY: process.env.MERCADO_PAGO_PUBLIC_KEY,
+    CLOUD_URL: process.env.CLOUD_URL,
   },
   images: {
-    domains: ['c1.neweggimages.com', 'lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
-  }
+    domains: ['res.cloudinary.com', 'c1.neweggimages.com', 'lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+  },
 };
 
 module.exports = nextConfig;

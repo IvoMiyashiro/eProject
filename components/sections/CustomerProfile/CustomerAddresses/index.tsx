9db037b78dcb0f70877cc5e39f75665eda2f8bb3
Ivan <ivo.miyashiro1@gmail.com>
2022-05-29 15:20:10 -0300

    add CustomerAddress

--- components/sections/CustomerProfile/CustomerAddresses/index.tsx
+++ components/sections/CustomerProfile/CustomerAddresses/index.tsx
@@ -3,8 +3,9 @@ import { useContext, useState } from 'react';
 import { AuthContext } from 'context';
 import { useAddress } from 'hooks';
 
+import { createAddress } from 'services';
 import { ArrowRightIcon } from 'components/icons';
-import { AddressCard, Button, CreateNewAddressForm, Modal } from 'components/ui';
+import { AddressCard, Button, AddressForm, Modal } from 'components/ui';
 
 import { lightTheme } from 'styles';
 import { H3, Wrapper,  Div, P, AddressList } from './styles';
@@ -12,35 +13,41 @@ import { H3, Wrapper,  Div, P, AddressList } from './styles';
 export const CustomerAddresses = () => {
 
   const { customer } = useContext(AuthContext);
-  const { addresses } = useAddress({ uid: customer?.id });
-  const [isAddNewAddressModalOpen, setAddNewAddressModalOpen] = useState(false);
+  const { addresses, setAddresses } = useAddress({ uid: customer?.id });
+  const [isAddressFormModalOpen, setAddressFormModalOpen] = useState(false);
 
   return (
     <div>
       <H3>Addresses</H3>
       <Div>
-        <AddressList>
-          {
-            (addresses?.length !== 0)
-              &&
-              addresses?.map(({ id, address, zip, province, locality }) => {
+        {
+          (addresses?.length !== 0)
+          &&
+          <AddressList >
+            {
+              addresses?.map(({ id, address, zip, province, locality, additional_info }) => {
                 return (
-                  <AddressCard 
+                  <AddressCard
                     key={id}
+                    address_id={id}
+                    customer_id={customer!.id}
                     address={address}
                     zip={zip}
                     province={province}
                     locality={locality}
+                    additionalInfo={additional_info}
+                    handleAddresses={setAddresses}
                   />
                 );
               })
-          }
-        </AddressList>
+            }
+          </AddressList>
+        }
         <Button
           bgColor={lightTheme.color_neutral_1}
           bRadius="4px"
           textColor={lightTheme.color_ui_text_main}
-          onClick={() => setAddNewAddressModalOpen(true)}
+          onClick={() => setAddressFormModalOpen(true)}
         >
           <Wrapper>
             <P>Add new address</P>
@@ -49,10 +56,15 @@ export const CustomerAddresses = () => {
         </Button>
       </Div>
       {
-        isAddNewAddressModalOpen
+        isAddressFormModalOpen
         &&
-        <Modal handleCloseChildren={() => setAddNewAddressModalOpen(false)}>
-          <CreateNewAddressForm handleAddNewAddressModalOpen={setAddNewAddressModalOpen}/>
+        <Modal handleCloseChildren={() => setAddressFormModalOpen(false)}>
+          <AddressForm
+            type="create"
+            onSubmit={createAddress}
+            handleAddressModalOpen={setAddressFormModalOpen}
+            handleAddresses={setAddresses}
+          />
         </Modal>
       }
     </div>

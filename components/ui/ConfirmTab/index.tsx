d47d6d29ce40defd1a546817b24f0024cfe8d3bd
Ivan <ivo.miyashiro1@gmail.com>
2022-05-29 15:19:36 -0300

    add modal to the component

--- components/ui/ConfirmTab/index.tsx
+++ components/ui/ConfirmTab/index.tsx
@@ -1,9 +1,11 @@
 import { Button } from 'components/ui';
 
 import { lightTheme } from 'styles';
+import { Modal } from '../Modal';
 import { Div, Wrapper, H2, P, Section } from './styles';
 
 interface Props {
+  isOpen: boolean;
   title: string;
   text: string;
   mainButtonColor: string;
@@ -13,9 +15,11 @@ interface Props {
   secondButtonTextColor?: string;
   onConfirm: any;
   onCancel: () => void;
+  handleOpen: (value: boolean) => void;
 }
 
 export const ConfirmTab = ({
+  isOpen,
   title,
   text,
   mainButtonColor,
@@ -25,33 +29,42 @@ export const ConfirmTab = ({
   secondButtonTextColor = lightTheme.color_ui_text_main,
   onConfirm,
   onCancel,
+  handleOpen,
 }: Props) => {
   return (
-    <Div>
-      <H2>{ title }</H2>
-      <P>{ text }</P>
-      <Section>
-        <Wrapper>
-          <Button
-            bRadius='4px'
-            bgColor={mainButtonColor}
-            textColor={mainButtonTextColor}
-            onClick={onConfirm}
-          >
-            { mainButtonChildren }
-          </Button>
-        </Wrapper>
-        <Wrapper>
-          <Button
-            bRadius='4px'
-            bgColor={secondButtonColor}
-            textColor={secondButtonTextColor}
-            onClick={onCancel}
-          >
-            Cancel
-          </Button>
-        </Wrapper>
-      </Section>
-    </Div>
+    <>
+      {
+        isOpen
+        &&
+        <Modal handleCloseChildren={() => handleOpen(false)}>
+          <Div>
+            <H2>{ title }</H2>
+            <P>{ text }</P>
+            <Section>
+              <Wrapper>
+                <Button
+                  bRadius='4px'
+                  bgColor={mainButtonColor}
+                  textColor={mainButtonTextColor}
+                  onClick={onConfirm}
+                >
+                  { mainButtonChildren }
+                </Button>
+              </Wrapper>
+              <Wrapper>
+                <Button
+                  bRadius='4px'
+                  bgColor={secondButtonColor}
+                  textColor={secondButtonTextColor}
+                  onClick={onCancel}
+                >
+                  Cancel
+                </Button>
+              </Wrapper>
+            </Section>
+          </Div>
+        </Modal>
+      }
+    </>
   );
 };

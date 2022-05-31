5fe1ab563edc428abb74ce9fdbf36c4bbc14e02f
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-05-31 18:06:49 -0300

    add put and delete endpoints

--- pages/api/customer/[uid]/address.ts
+++ pages/api/customer/[uid]/address.ts
@@ -13,6 +13,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
   case 'POST':
     return createAddress(req, res);
+  
+  case 'PUT':
+    return updateAddress(req, res);
 
   case 'DELETE':
     return deleteAddress(req, res);
@@ -37,7 +40,7 @@ const createAddress = async (req: NextApiRequest, res: NextApiResponse<Data>) =>
 
     return res.status(200).json({
       ok: true,
-      address: rows,
+      address: rows[0],
       message: 'Address created.'
     });
     
@@ -75,6 +78,50 @@ const getAddress = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   }
 };
 
-const deleteAddress = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  
+const updateAddress = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { address_id, address, zip, province, locality, additional_info } = req.query;
+
+  const query = 'UPDATE customer_address SET address = $1, zip = $2, province = $3, locality = $4, additional_info = $5 WHERE customer_id = $6 RETURNING *';
+  const value = [address, zip, province, locality, additional_info, address_id];
+
+  try {
+    const { rows } = await db.conn.query(query, value);
+
+    return res.status(200).json({
+      ok: true,
+      address: rows[0]
+    });
+    
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      ok: false,
+      message: 'Internal server error.'
+    });
+  }
+};
+
+const deleteAddress = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { address_id } = req.body;
+
+  const query = 'DELETE FROM customer_address WHERE id = $1';
+  const value = [address_id];
+
+  try {
+    await db.conn.query(query, value);
+
+    return res.status(200).json({
+      ok: true,
+      message: 'Address successfully deleted.'
+    });
+    
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      ok: false,
+      message: 'Internal server error.'
+    });
+  }
 };

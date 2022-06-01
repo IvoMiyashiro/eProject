d2089301a8300491df4660e600528fcadb766670
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-05-31 21:35:24 -0300

    add testing user env

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     BASE_URL: process.env.BASE_URL,
     MERCADO_PAGO_PUBLIC_KEY: process.env.MERCADO_PAGO_PUBLIC_KEY,
     CLOUD_URL: process.env.CLOUD_URL,
+    TESTING_USER_ID: process.env.TESTING_USER_ID,
   },
   images: {
     domains: ['res.cloudinary.com', 'c1.neweggimages.com', 'lh3.googleusercontent.com', 'avatars.githubusercontent.com'],

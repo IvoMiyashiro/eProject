8af09426b55b5fc42be752c7be65278820d5f40d
Ivan <ivo.miyashiro1@gmail.com>
2022-06-02 12:12:35 -0300

    add dashboard page

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import { DashboardLayout } from 'components/layouts';
+
+const DashboardPage: NextPage = () => {
+  return (
+    <DashboardLayout>
+      <p> hola </p>
+    </DashboardLayout>
+  );
+};
+
+export default DashboardPage;
+

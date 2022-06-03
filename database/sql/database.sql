ab0b5ba207c3359587d7d41d9e6c3c95136266ce
Ivan <ivo.miyashiro1@gmail.com>
2022-06-03 10:27:17 -0300

    add total_purchases, created_at and updated_at to customer table

--- database/sql/database.sql
+++ database/sql/database.sql
@@ -87,7 +87,9 @@ CREATE TABLE IF NOT EXISTS customer (
   email VARCHAR(100) NOT NULL UNIQUE,
   password VARCHAR(100) NOT NULL,
   profile_image VARCHAR(2048),
-  
+  total_purchases INT,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   role user_roles
 );
 

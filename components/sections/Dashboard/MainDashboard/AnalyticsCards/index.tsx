a7acf1bee87c6bfbb8814ccac32677a8bcfbcf6c
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-06-07 10:10:53 -0300

    fix order of cards and add "add new product" card

--- components/sections/Dashboard/MainDashboard/AnalyticsCards/index.tsx
+++ components/sections/Dashboard/MainDashboard/AnalyticsCards/index.tsx
@@ -1,4 +1,4 @@
-import { BookmarkIcon, ReviewIcon, TagIcon, UserIcon } from 'components/icons';
+import { BookmarkIcon, PlusIcon, ReviewIcon, TagIcon, UserIcon } from 'components/icons';
 import { AnalyticsCard } from 'components/ui';
 import styled from 'styled-components';
 
@@ -11,6 +11,7 @@ export const AnalyticsCards = () => {
         icon={<BookmarkIcon width="40px" height="40px" color="#83AE89"/>}
         title="Total Orders"
         data={'79'}
+        path='/dashboard/orders'
       />
       <AnalyticsCard
         background_color="#F4F2FF"
@@ -18,6 +19,15 @@ export const AnalyticsCards = () => {
         icon={<TagIcon width="45px" height="45px" color="#867DB1"/>}
         title="Total Products"
         data={'79'}
+        path='/dashboard/products'
+      />
+      <AnalyticsCard
+        background_color="#ECFBFF"
+        text_color="#42717F"
+        icon={<UserIcon width="40px" height="40px" color="#42717F"/>}
+        title="Total Users"
+        data={'79'}
+        path='/dashboard/users'
       />
       <AnalyticsCard
         background_color="#FFF3EC"
@@ -25,20 +35,15 @@ export const AnalyticsCards = () => {
         icon={<ReviewIcon width="40px" height="40px" color="#AF8C7D"/>}
         title="Total Reviews"
         data={'79'}
+        path='/dashboard/reviews'
       />
       <AnalyticsCard
-        background_color="#ECFBFF"
-        text_color="#42717F"
-        icon={<UserIcon width="40px" height="40px" color="#42717F"/>}
-        title="Total Users"
-        data={'79'}
-      />
-      <AnalyticsCard
-        background_color="#ECFBFF"
-        text_color="#42717F"
-        icon={<UserIcon width="40px" height="40px" color="#42717F"/>}
-        title="Total Users"
-        data={'79'}
+        background_color="#F4F2FF"
+        text_color="#867DB1"
+        icon={<PlusIcon width="40px" height="40px" color="#867DB1"/>}
+        title="New"
+        data="Product"
+        path='/dashboard/products/new'
       />
     </Div>
   );

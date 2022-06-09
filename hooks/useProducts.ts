a0f3d1e4d7c00d6d9ca2fc69f3a2d3d6d4aa4922
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-06-09 18:38:44 -0300

    add formatter to sortby and orderby

--- hooks/useProducts.ts
+++ hooks/useProducts.ts
@@ -5,28 +5,47 @@ interface Props {
   limit: number;
   offset: number;
   search: string;
+  sortBy: string;
 }
 
-export const useProducts = ({ limit, offset, search }: Props) => {
+export const useProducts = ({ limit, offset, sortBy, search }: Props) => {
 
   const [productsList, setProductsList] = useState<IProduct[] | []>([]);
   const [totalCount, setTotalCount] = useState(0);
   const [isLoading, setLoading] = useState(true);
+  const formatedSortBy = formatSortBy(sortBy);
+  const formatedOrderBy = formatOrderBy(sortBy);
 
   useEffect(() => {
     setLoading(true);
-    fetch(`/api/products?limit=${ limit }&offset=${ offset }&search=${ search }`)
+    fetch(`/api/products?limit=${ limit }&offset=${ offset }&search=${ search }&sortBy=${ formatedSortBy }&orderBy=${ formatedOrderBy }`)
       .then(resp => resp.json())
       .then(({ products, totalCount }) => {
         setProductsList(products);
         setTotalCount(totalCount);
         setLoading(false);
       });
-  }, [limit, offset, search]);
+  }, [limit, offset, formatedSortBy, formatedOrderBy, search]);
 
   return { 
     isLoading,
     productsList,
     totalCount
   };
+};
+
+const formatSortBy = (value: string) => {
+  if (value === 'Highest price' || value === 'Lowest price') {
+    return 'price';
+  }
+
+  return 'title';
+};
+
+const formatOrderBy = (value: string) => {
+  if (value === 'A - Z' || value === 'Lowest price') {
+    return 'ASC';
+  }
+
+  return 'DESC';
 };

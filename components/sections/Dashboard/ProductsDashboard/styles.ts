22fa0861c3a58a3a656377fed85a316981fdca21
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-06-09 18:44:22 -0300

    add Input styles

--- components/sections/Dashboard/ProductsDashboard/styles.ts
+++ components/sections/Dashboard/ProductsDashboard/styles.ts
@@ -10,3 +10,13 @@ export const Wrapper = styled.div`
   grid-template-columns: 1fr 100px 150px 150px;
   margin: 2em 0;
 `;
+
+export const Input = styled.input`
+  border-radius: 4px;
+  border: 1px solid ${props => props.theme.color_neutral_0};
+  height: 100%;
+  outline: none;
+  padding: 0 1em;
+  transition: 0.2s ease-in-out;
+  width: 100%;
+`;

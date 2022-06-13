e0d41069c086bbcd0d114af864d7386b2192bede
Ivan <ivo.miyashiro1@gmail.com>
2022-06-13 16:13:12 -0300

    add font-familiy 'Inter'

--- components/ui/Inputs/InputTextarea/styles.ts
+++ components/ui/Inputs/InputTextarea/styles.ts
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 export const TextArea = styled.textarea`
   background-color: transparent;
   border: none;
+  font-family: 'Inter';
   font-size: 0.9rem;
   outline: none;
-  resize: none;
   overflow: hidden;
+  resize: none;
 `;

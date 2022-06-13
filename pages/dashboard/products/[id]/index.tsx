df089e8ec07c98060f058325c8e02160ad1ea907
Ivan <ivo.miyashiro1@gmail.com>
2022-06-13 16:13:30 -0300

    add dashboard product page

---
+++ pages/dashboard/products/[id]/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import { DashboardLayout } from 'components/layouts';
+
+
+const DashboardPage: NextPage = () => {
+  return (
+    <DashboardLayout sectionTitle="Products" pageTitle=' | Products'>
+    </DashboardLayout>
+  );
+};
+
+export default DashboardPage;

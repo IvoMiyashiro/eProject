08d3bdf8c27cf77f6beb615412c00aba25a3e715
Ivan <ivo.miyashiro1@gmail.com>
2022-06-25 12:43:25 -0300

    delete labels inputs

--- components/sections/Dashboard/NewProductDashboard/ProductOrganization/index.tsx
+++ components/sections/Dashboard/NewProductDashboard/ProductOrganization/index.tsx
@@ -1,11 +1,10 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction } from 'react';
 
-import { IInputControl, INPUT_CONTROL_INIT_STATE } from 'helpers/input_control_init_state';
+import { IInputControl } from 'helpers/input_control_init_state';
 
 import { useProductOrganization } from 'hooks';
 
 import { InputSelect } from 'components/ui';
-import { LabelsForm } from './LabelsForm';
 
 import { Div, H3 } from '../ProductPrice/styles';
 import { Section } from '../styles';
@@ -13,23 +12,18 @@ import { Section } from '../styles';
 interface Props {
   productCategory: IInputControl;
   productVendor: IInputControl;
-  productLabelsList: string[];
   handleProductCategory: Dispatch<SetStateAction<IInputControl>>;
   handleProductVendor: Dispatch<SetStateAction<IInputControl>>;
-  handleProductLabelsList: Dispatch<SetStateAction<string[]>>;
 }
 
 export const ProductOrganization = ({
   productCategory,
   productVendor,
-  productLabelsList,
   handleProductCategory,
   handleProductVendor,
-  handleProductLabelsList
 }: Props) => {
 
   const { categories, vendors } = useProductOrganization();
-  const [prorductLabel, setProductLabel] = useState(INPUT_CONTROL_INIT_STATE);
 
   return (
     <Div>
@@ -46,12 +40,6 @@ export const ProductOrganization = ({
           options={vendors as string[]}
           state={productVendor}
           handleStateValue={handleProductVendor}
-        />
-        <LabelsForm 
-          productLabel={prorductLabel}
-          productLabelsList={productLabelsList}
-          handleProductLabel={setProductLabel}
-          handleProductLabelsList={handleProductLabelsList}
         />
       </Section>
     </Div>
